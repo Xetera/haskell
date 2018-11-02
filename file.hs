a0236874d83e9febfbed4c7426d440126b6eeffd
ilocereal <mythologyali@gmail.com>
2018-11-01 18:58:32 -0700

    file finding modules

--- file.hs
+++ file.hs
@@ -1,15 +1,33 @@
-import System.Directory
+import System.Directory (listDirectory)
 import System.IO
-import Control.Monad (when)
+import Control.Monad
+import Data.List
 
 fileName = "something.txt"
 
-overwriteWith :: String -> (String -> String) -> IO()
-overwriteWith name modifier = do
+readFileLines :: String -> IO [String]
+readFileLines target = lines <$> readFile target
+
+
+overwriteWith :: (String -> String) -> String -> IO()
+overwriteWith modifier name = do
     content <- readFile name
-    let replacer = modifier content
-    when (length content > 0) $
-        writeFile fileName replacer
+    length content `seq` writeFile name $ modifier content
+
+
+findFiles :: FilePath -> String -> IO [FilePath]
+findFiles target directory = let containsTarget = isInfixOf target in 
+    filter containsTarget <$> listDirectory directory 
+
+
+findFile :: FilePath -> String -> IO (Maybe FilePath)
+findFile target directory = head' <$> findFiles target directory
+
+
+head' [] = Nothing
+head' (x:xs) = Just x
+
 
 main = do
-    overwriteWith fileName (head . lines)
+    findFiles "." "." 
+    
